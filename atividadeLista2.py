0aa92995d31e8ef5c5338bc4bd6b7d6ca05c67f0
JuanLucca846 <lucca470@gmail.com>
2024-03-05 15:22:31 -0300

    atividade

---
+++ atividadeLista2.py
@@ -0,0 +1,14 @@
+listaMusicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+
+tamanhoLista = len(listaMusicos)
+print(str(tamanhoLista) + "\n")
+
+indiceDois = (listaMusicos[2])
+print(str(indiceDois) + "\n")
+
+indiceNove = (listaMusicos[9])
+print(str(indiceNove) + "\n")
+
+indiceQuatorze = (listaMusicos[14])
+print(str(indiceQuatorze) + "\n")
