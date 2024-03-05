5979da441f5dbeeec2af4058313129ad7b223f60
JuanLucca846 <lucca470@gmail.com>
2024-03-05 15:27:25 -0300

    atividade

---
+++ atividadeLista3.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for i in range(8):
+    print("Temos " + lista_produtos[i] + " à venda!\n")
